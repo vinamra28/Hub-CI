33afd7288650e1ac18b95e4a756e2b48c8270ad2
vinamra28 <vinjain@redhat.com>
2021-01-29 15:16:51 +0530

    Modified the Workflow for UI

--- ui/tasks.sh
+++ ui/tasks.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-ns=${NAMESPACE:-"hub"}
-oc=${oc:-""}
+ns=${NAMESPACE:-"hub-ci"}
+oc=${oc:-"1"}
 
 echo ${ns}
 
+oc delete namespace ${ns} --ignore-not-found
+
+oc create namespace ${ns}
+
 kubectl apply -f secret.yaml -n ${ns}
+
+kubectl create configmap kubeconfig --from-file="${HOME}/Downloads/kubeconfig" -n ${ns}
+
+kubectl apply -f tokens-configmap.yaml -n ${ns}
 
 kubectl apply -f serviceaccount.yaml -n ${ns}
 
@@ -15,9 +23,15 @@ kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/
 
 kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.2/buildah.yaml -n ${ns}
 
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/kubernetes-actions/0.1/kubernetes-actions.yaml -n ${ns}
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/codecov/0.1/codecov.yaml -n ${ns}
+
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/replace-tokens/0.1/replace-tokens.yaml -n ${ns}
+
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/kubernetes-actions/0.2/kubernetes-actions.yaml -n ${ns}
 
 [[ ! -z ${oc} ]] && \
 oc adm policy add-scc-to-user privileged system:serviceaccount:${ns}:quay-login
+
+kubectl apply -f pipeline.yaml -n ${ns}
 
 kubectl apply -f pipelinerun.yaml -n ${ns}
